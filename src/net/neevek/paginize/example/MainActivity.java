34cf56488f7c8ddbe607c790cd776da9b2297883
neevek <i@neevek.net>
2014-03-29 01:31:55 +0800

    use SlidePageAnimationManager by default for the example

--- src/net/neevek/paginize/example/MainActivity.java
+++ src/net/neevek/paginize/example/MainActivity.java
@@ -2,11 +2,11 @@ package net.neevek.paginize.example;
 
 import android.os.Bundle;
 import net.neevek.paginize.lib.PageActivity;
-import net.neevek.paginize.lib.anim.ZoomPageAnimationManager;
+import net.neevek.paginize.lib.anim.SlidePageAnimationManager;
 import net.neevek.paginize.lib.annotation.InjectPageAnimationManager;
 
-@InjectPageAnimationManager(ZoomPageAnimationManager.class)
-//@InjectPageAnimationManager(SlidePageAnimationManager.class)
+//@InjectPageAnimationManager(ZoomPageAnimationManager.class)
+@InjectPageAnimationManager(SlidePageAnimationManager.class)
 public class MainActivity extends PageActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
