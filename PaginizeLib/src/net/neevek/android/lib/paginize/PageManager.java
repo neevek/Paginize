neevek/Paginize

commit f2dbecb84ec45427f76bf02fc29fa1a8b2c9dd2e

refacoted PageManager to better support per-Page transition animations. added ViewWrapper.onMenuPressed() to handle press on the menu button

PaginizeLib/src/net/neevek/android/lib/paginize/PageManager.java
@@ -119,7 +119,7 @@ public class PageManager {
       animationDuration = mPageAnimator.getAnimationDuration();
     }
     if (animated && animationDuration != -1) {
-      newPage.getView().postDelayed(new Runnable() {
+      newPage.postDelayed(new Runnable() {
         @Override
         public void run() {
           hideOldPageIfNeeded(oldPage, newPage);
@@ -310,32 +310,31 @@ public class PageManager {
       }
     }
 
-    mContainerView.removeView(removedPage.getView());
-    removedPage.onDetach();
-
-    mCurPage = prevPage;
-
     int animationDuration = removedPage.getAnimationDuration();
     if (animationDuration == -1 && mPageAnimator != null) {
       animationDuration = mPageAnimator.getAnimationDuration();
     }
     if (animated && animationDuration != -1) {
-      removedPage.getView().postDelayed(new Runnable() {
+      removedPage.postDelayed(new Runnable() {
         @Override
         public void run() {
-          removedPage.onHidden();
-
-          if (prevPage != null) {
-            prevPage.onUncovered(removedPage.getReturnData());
-          }
+          doFinalWorkForPopPageInternal(removedPage, prevPage);
         }
       }, animationDuration);
-    } else {
-      removedPage.onHidden();
 
-      if (prevPage != null) {
-        prevPage.onUncovered(removedPage.getReturnData());
-      }
+    } else {
+      doFinalWorkForPopPageInternal(removedPage, prevPage);
+    }
+  }
+
+  private void doFinalWorkForPopPageInternal(Page removedPage, Page prevPage) {
+    mContainerView.removeView(removedPage.getView());
+    removedPage.onDetach();
+    removedPage.onHidden();
+    mCurPage = prevPage;
+
+    if (prevPage != null) {
+      prevPage.onUncovered(removedPage.getReturnData());
     }
   }
 
@@ -408,7 +407,12 @@ public class PageManager {
 
   public boolean onKeyDown(int keyCode, KeyEvent event) {
     if (mCurPage != null) {
-      return mCurPage.onKeyDown(keyCode, event);
+      if (keyCode == KeyEvent.KEYCODE_MENU && event.getRepeatCount() == 0) {
+        return mCurPage.onMenuPressed();
+
+      } else {
+        return mCurPage.onKeyDown(keyCode, event);
+      }
     }
     return false;
   }

PaginizeLib/src/net/neevek/android/lib/paginize/ViewWrapper.java
@@ -214,6 +214,16 @@ public abstract class ViewWrapper {
   }
 
   /**
+   * onMenuPressed is called when KeyEvent for onKeyDown() is KEYCODE_MENU, only the current
+   * page(on top of the stack) receives this call
+   *
+   * @see net.neevek.android.lib.paginize.PageManager
+   */
+  public boolean onMenuPressed() {
+    return false;
+  }
+
+  /**
    * onActivityResult mirrors Activity.onActivityResult, only the current
    * page(on top of the stack) receives this call
    *

commit 8b0238c064c8a7465f2c65c40a05be2a6d980636

fixed a bug I introduced in f2dbecb84ec45427f76bf02fc29fa1a8b2c9dd2e. mCurPage should be set to null immediately when last page is popped, even if there is an transition animation to play

PaginizeLib/src/net/neevek/android/lib/paginize/PageManager.java
@@ -326,6 +326,9 @@ public class PageManager {
       }
     }
 
+    mContainerView.removeView(removedPage.getView());
+    mCurPage = prevPage;
+
     int animationDuration = removedPage.getAnimationDuration();
     if (animationDuration == -1 && mPageAnimator != null) {
       animationDuration = mPageAnimator.getAnimationDuration();
@@ -344,10 +347,8 @@ public class PageManager {
   }
 
   private void doFinalWorkForPopPageInternal(Page removedPage, Page prevPage) {
-    mContainerView.removeView(removedPage.getView());
     removedPage.onDetach();
     removedPage.onHidden();
-    mCurPage = prevPage;
 
     if (prevPage != null) {
       prevPage.onUncovered(removedPage.getReturnData());